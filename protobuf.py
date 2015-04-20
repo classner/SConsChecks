c7aa7bd42bee1173e8c67594404b6153828dd17e
Christoph Lassner <mail@christophlassner.de>
2015-04-21 01:46:26 +0200

    Made protobuf checks compatible to older versions.

--- protobuf.py
+++ protobuf.py
@@ -48,8 +48,8 @@ def CheckProtobuf(context):
 #include <google/protobuf/message.h>
 #include <google/protobuf/repeated_field.h>
 #include <google/protobuf/extension_set.h>
-#include <google/protobuf/generated_enum_reflection.h>
-#include <google/protobuf/unknown_field_set.h>
+//#include <google/protobuf/generated_enum_reflection.h>
+//#include <google/protobuf/unknown_field_set.h>
 
 int main() {
   return 0;
